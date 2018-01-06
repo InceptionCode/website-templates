56e15c7e1f4a13f05f3e3597c8af1d8ab0af6330
Darrell Washington <darrellwjr.com@gmail.com>
2018-01-06 15:14:20 -0500

    "Finished navigation & started on About section"

--- js/template4.js
+++ js/template4.js
@@ -1,17 +1,48 @@
-const navToggle = document.querySelector(".fa-bars"),
-    exitToggle = document.querySelector(".fa-times");
+const navLarge = document.querySelector(".desktop-nav"),
+    navMobile = document.querySelector(".mobile-nav"),
+    navToggle = document.querySelector(".fa-bars"),
+    exitToggle = document.querySelector(".fa-times"),
+    navElements = document.querySelectorAll(".mobile-nav li");
 
-const displayNav = (e)=> {
+const changeNavBg = ()=> {
+    if (window.scrollY > 0) {
+        navLarge.style = "background: #fff";
+        navMobile.style = "background: #fff";
+    } else {
+        navLarge.style = "background: transparent";
+        navMobile.style = "background: transparent";
+    }
+};
+
+const toggleNavElements = action => {
+
+    navElements.forEach(element => {
+        const checkClassList = !element.classList.contains("home");
+        const checkAction = action === "show";
+        if (checkClassList && checkAction) {
+            element.classList.add("list-display");
+            element.classList.remove("dn");
+        } else if (checkClassList && !checkAction) {
+            element.classList.add("dn");
+            element.classList.remove("list-display");
+        }
+    });
+};
+
+const toggleNav = (e)=> {
     e.target.classList.add("dn");
     e.target.classList.remove("di");
     if (e.target === navToggle) {
         exitToggle.classList.add("di");
         exitToggle.classList.remove("dn");
+        toggleNavElements("show");
     } else {
         navToggle.classList.add("di");
         navToggle.classList.remove("dn");
+        toggleNavElements("hide");
     }
 };
 
-navToggle.onclick = function (e) {  displayNav(e); };
-exitToggle.onclick = function (e) {  displayNav(e); };
+window.onscroll = function () { changeNavBg(); };
+navToggle.onclick = function (e) {  toggleNav(e); };
+exitToggle.onclick = function (e) {  toggleNav(e); };
